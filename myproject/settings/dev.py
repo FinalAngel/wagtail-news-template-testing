57dcce05ee8cecb20d69cfd57931130f8b5010a2
Angelo Dini <angelo.dini@divio.ch>
2024-12-12 10:04:39 +0100

    first commit

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-og6+7xkg50yt!c@xri)kpys@xk^j)_w8c4bok(r^5@&p21(s@+"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
